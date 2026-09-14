a895a58712ee9d8f572a58988374c2cb088a164d
Cynthia Guzman Ramirez <cynguzmanramirez@gmail.com>
2026-09-14 01:15:00 -0500

    Began working on aesthetic layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import logo from './assets/TheBrewReviewLogo.png'
 import './App.css'
 
 
@@ -12,12 +13,28 @@ function App() {
 
   return(
     <div className="app">
-      <h1>BrewLog</h1>
+      <header className="site-header">
+        <div className="flex-row header-left">
+          <img src={logo} alt="The Brew Review logo" className="logo" />
+          <button className="btn tab-active">Discover</button>
+        </div>
+        <div className="flex-row header-right">
+          <button className="btn btn-text">Sign in</button>
+          <button className="btn btn-primary">Sign up</button>
+        </div>
+      </header>
+
+      <div className="search-section">
+        <div className="search-bar">
+          <span className="search-icon"></span>
+        </div>
+      </div>
+
       <p>Find your next coffee shop in Austin</p>
 
       <div className="shop-list">
-       {shops.map((shop) => (
-          <div key={shops.id} className = "shop-card">
+        {shops.map((shop) => (
+          <div key={shop.id} className="shop-card">
             <h2>{shop.name}</h2>
             <p>{shop.address}</p>
             <p>{shop.tags.join(', ')}</p>
